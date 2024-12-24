6da2d3a0ecc0ca95d70a7c2a95af13c4b93f233a
Денис Ковтун <dakislayergd@gmail.com>
2024-12-24 20:26:54 +0300

    Update and rename 3.1.c to 2.1.c

--- 2.1.c
+++ 2.1.c
@@ -4,40 +4,52 @@
 #include <float.h>
 #include <stdlib.h>
 
-/** 
+/
+ * @param AREA_FACE площадь одной грани
+ * @param TOTAL_AREA площадь полной поверхности
+ * @param VOLUME_CUBE обьем куба
+ */
+enum choice {AREA_FACE = 1, TOTAL_AREA = 2, VOLUME_CUBE = 3};
+ 
+/
+ * @brief функция печатающая выбор пользователю
+ */
+void print_menu(void);
+
+/ 
  * @brief функция, считывающая число из буфера обмена
  * @return возвращает ошибку и закрывает в случае, если число введено некорректно
  */
 double input(void);
 
-/**
+/
  * @brief функция, проверяющая число на положительность
  * @return возвращает ошибку и закрывает программу в случае, если число неположительное
  */
 double pozitiv_double(void);
 
-/**
+/
  * @brief функция, рассчитывающая площадь грани куба
  * @param length длинна ребра куба
  * @return возвращает рассчитыанное значение площади грани куба
  */
 double area_of_the_face(const double length);
 
-/**
+/
  * @brief функция, рассчитывающая площадь полной поверхности
  * @param length длинна ребра куба
  * @return возвращает рассчитанное значение площади полной поверхности
  */
 double total_surface_area(const double length);
 
-/**
+/
  * @brief функция, рассчитыывающая объем куба
  * @param length длинна ребра куба
  * @return возвращает рассчитанное значение объема куба
  */
 double volume_of_the_cube(const double length);
 
-/**
+/
  * @brief точка входа в программу
  * @return 0 в случае успеха
  */
@@ -45,18 +57,36 @@ int main(void)
 { 
     printf("Please enter сubas length\n");
     const double length = pozitiv_double();
-    printf("the program calculates the area of the face\n");
-    
-    double area1 = area_of_the_face(length);
-    printf("The area of the face: %.3lf\n", area1);
+    print_menu();
+    int dependet = (int)input();
 
-    double area2 = total_surface_area(length);
-    printf("total surface area: %.3lf\n", area2);
-    
-    double volume = volume_of_the_cube(length);
-    printf("volume of the cube: %.3lf\n", volume);
+    switch (dependet) {
+    case AREA_FACE:
+            printf("The area of the face: %.3lf\n", area_of_the_face(length));
+            break;
+
+        case  TOTAL_AREA :
+            printf("The total surface area: %.3lf\n", total_surface_area(length));
+            break;
+
+        case VOLUME_CUBE:
+            printf("The volume of the cube: %.3lf\n", volume_of_the_cube(length));
+            break;
+
+        default:
+            perror("Invalid operation selected.\n");
+            exit(EXIT_FAILURE);
+    }
 
     return 0;
+}
+
+void print_menu(void)
+{ 
+    printf("Select an operation:\n");
+    printf("1 - Area of the face\n");
+    printf("2 - Total surface area\n");
+    printf("3 - Volume of the cube\n");
 }
 
 double input(void)
@@ -98,4 +128,3 @@ double total_surface_area(const double length)
 double volume_of_the_cube(const double length)
 { 
     return pow(length, 3);
-}
